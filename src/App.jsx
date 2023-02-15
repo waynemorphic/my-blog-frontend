478b6bfedde96f7e78a348801294020699687bd8
waynemorphic <wayneklyde@gmail.com>
2023-02-15 18:10:48 +0300

    Components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Admin from './Pages/Admin'
+
+class App extends Component {
+    render(){
+        return (
+            <div>
+                <Admin />
+            </div>            
+        )
+    }
+}
+
+export default App
